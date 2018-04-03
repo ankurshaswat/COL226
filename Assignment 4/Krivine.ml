1f5e53181669634d4c956c34f9d2628ecbef3242
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2018-04-03 23:24:38 +0530

    SECD completed

--- Assignment 4/Krivine.ml
+++ Assignment 4/Krivine.ml
@@ -10,8 +10,9 @@ and env = Func of (string * closure) list;;
 let rec lookup (env,xx) = match (env,xx) with
   | (Func((x,closs)::xs),y) -> if(x=y) then closs else lookup(Func(xs),y);;
 
-let evalKrivine state closureStack = match (state,closureStack) with
-  | (Clos(env,Var(x)),s) -> (lookup(env,x),s)
-  | (Clos(Func(ll),Function(x,e1)),c::s) -> (Clos(Func((x,c)::ll),e1),s)
-  | (Clos(env,Call(e1,e2)),s) -> (Clos(env,e1),Clos(env,e2)::s)
+let rec evalKrivine state closureStack = match (state,closureStack) with
+  | (Clos(env,))
+  | (Clos(env,Var(x)),s) -> evalKrivine (lookup(env,x)) (s)
+  | (Clos(Func(ll),Function(x,e1)),c::s) -> evalKrivine (Clos(Func((x,c)::ll),e1)) (s)
+  | (Clos(env,Call(e1,e2)),s) -> evalKrivine (Clos(env,e1)) (Clos(env,e2)::s)
 ;;
--- Assignment 4/SECD.ml
+++ Assignment 4/SECD.ml
@@ -1,4 +1,5 @@
 exception Error;;
+open List;;
 
 type exp = Const of int
          | Plus of exp * exp
@@ -26,11 +27,13 @@ type opcode = VAR of string
             | EQUAL
             | TT
             | FF
-            | TUPLE of opcode list list
+            | TUPLE of int
             | IF
-            | COND of opcode list * (opcode list)
             | CLOSURE of string * opcode list
             | RET
+            | STORE_ENV
+            | POP_ENV
+            | RESTORE_ENV
             | APP;;
 
 type answer = Const of int | T | F | Tuple of answer list | VClosure of ((string * answer) list) * string  *  opcode list;;
@@ -48,6 +51,10 @@ let rec invertMap data functionList = match functionList with
   | [] -> []
   | x::xs -> (x data)::(invertMap data xs);;
 
+let rec form_single_list a = match a with
+  | [] -> [POP_ENV]
+  | x::xs -> x @ [RESTORE_ENV] @ (form_single_list xs)
+
 let rec compile e = match e with
   |Var(s)->[VAR(s)]
   |Const(n) -> [CONST(n)]
@@ -57,14 +64,23 @@ let rec compile e = match e with
   |F -> [FF]
   |Equal(e1,e2) -> compile(e1)@compile(e2)@[EQUAL]
   |GreaterThan(e1,e2) -> compile(e1)@compile(e2)@[GREATERTHAN]
-  |Tuple(l) -> [TUPLE(map compile l)]
-  |IfThenElse(e0,e1,e2) -> [COND(compile e1,compile e2)]@compile(e2)@[IF]
+  |Tuple(l) -> [STORE_ENV]@(form_single_list (map compile l))@[TUPLE(List.length l)]
+  |IfThenElse(e0,e1,e2) -> [STORE_ENV]@(compile e2)@[RESTORE_ENV]@(compile e1)@[RESTORE_ENV]@compile(e0)@[RESTORE_ENV;POP_ENV]@[IF]
   |Function (s,e) -> [CLOSURE(s,compile(e)@[RET])]
   | Call (e1,e2) -> compile(e1)@compile(e2)@[APP]
 ;;
 
+let rec pop_n s n = match (s,n) with
+| (s,0) -> []
+| (x::xs,n) -> [x] @ (pop_n xs (n-1));;
+
+let rec pop_n_get_stack s n = match (s,n) with
+| (s,0) -> s
+| (x::xs,n) -> (pop_n_get_stack xs (n-1));;
+
 
 let rec evalSECD stack environment opcodeL dump = match (stack,environment,opcodeL,dump) with
+  | (a::s,e,[],Dump(d)) -> a
   | (s,e,CONST(n)::c,Dump(d)) -> evalSECD (Const(n)::s) e c (Dump(d))
   | (s1,Table(xx),VAR(s)::c,Dump(d)) -> evalSECD ((List.assoc s xx)::s1) (Table(xx)) c (Dump(d))
   | (Const(a)::Const(b)::s,e,PLUS::c,Dump(d)) -> evalSECD (Const(a+b)::s) e c (Dump(d))
@@ -73,12 +89,14 @@ let rec evalSECD stack environment opcodeL dump = match (stack,environment,opcod
   | (Const(a)::Const(b)::s,e,EQUAL::c,Dump(d)) -> if (b=a) then evalSECD (T::s) e c (Dump(d)) else evalSECD (F::s) e c (Dump(d))
   | (s,e,TT::c,Dump(d)) -> evalSECD (T::s) e c (Dump(d))
   | (s,e,FF::c,Dump(d)) -> evalSECD (F::s) e c (Dump(d))
-  | (s,e,TUPLE(l)::c,Dump(d)) -> evalSECD (Tuple(invertMap (Dump([])) (map (evalSECD [] e) l))::s) e c (Dump(d))
+  | (s,e,TUPLE(n)::c,Dump(d)) -> evalSECD (Tuple(pop_n s n)::(pop_n_get_stack s n)) e c (Dump(d))
   | (T::tru::fals::s,e,IF::c,Dump(d)) ->  evalSECD (tru::s) e c (Dump(d))
   | (F::tru::fals::s,e,IF::c,Dump(d)) ->  evalSECD (fals::s) e c (Dump(d))
-  | (s,e,COND(a,b)::c,Dump(d)) -> evalSECD ((evalSECD [] e a (Dump([])))::(evalSECD [] e b (Dump([])))::s) e c (Dump(d))
   | (s,Table(e),CLOSURE(x,opcodeL)::c,Dump(d)) -> evalSECD (VClosure(e,x,opcodeL)::s) (Table(e)) c (Dump(d))
   | (a::sPrime,Table(ePrime),RET::cPrime,Dump((s,Table(e),c)::d)) -> evalSECD (a::s) (Table(e)) c (Dump(d))
   | (a::VClosure(e,y,c)::s,Table(subs),APP::cPrime,Dump(d)) -> evalSECD [] (Table((y,a)::subs)) c (Dump((s,Table(subs),cPrime)::d))
+  | (s,e,STORE_ENV::c,Dump(d)) -> evalSECD s e c (Dump(([],e,[])::d))
+  | (s,e,RESTORE_ENV::c,Dump((sP,eP,cP)::d)) -> evalSECD s eP c (Dump((sP,eP,cP)::d))
+  | (s,e,POP_ENV::c,Dump((sP,eP,cP)::d)) -> evalSECD s e c (Dump(d))
   | _ -> raise Error
 ;;
