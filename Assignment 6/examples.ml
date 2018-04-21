6230d7786046b1e46d86455cced5464fe084297a
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2018-04-21 21:15:14 +0530

    Added More Examples

--- Assignment 6/examples.ml
+++ Assignment 6/examples.ml
@@ -1,11 +1,14 @@
 let f1 = Fact(Function("brother",[Function("ankit",[]);Function("ankit",[])]));;
-let f1 = Fact(Function("brother",[Function("ankur",[]);Function("ankit",[])]));;
-let f2 = Fact(Function("brother",[Function("ankur",[]);Function("swapnil",[])]));;
+let f2 = Fact(Function("brother",[Function("ankur",[]);Function("ankit",[])]));;
+let f22 = Fact(Function("brother",[Function("ankur",[]);Function("swapnil",[])]));;
 
-let f3 = Function("brother",[Function("ankur",[]);Function("swapnil",[])]);;
+let prog = [f1;f2;f22];;
 
-let prog = [f1;f2];;
-unify [f3] prog;;
+(* let f3 = Function("brother",[Function("ankur",[]);Function("swapnil",[])]);; *)
+(* unify [f3] prog;; *)
 
-let f3 = Function("brother",[Function("ankur",[]);V(Var("X"))]);;
+(* unify [f3] prog;; *)
+(* let f3 = Function("brother",[Function("ankur",[]);V(Var("X"))]);; *)
+
+let f3 = Function("brother",[V(Var("Y"));V(Var("X"))]);;
 unify [f3] prog;;
